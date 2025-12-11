c895fa73fcca47309dfeca24744e15d046c538e8
rajesh-raveendran <45645689+rajesh-raveendran@users.noreply.github.com>
2025-12-11 18:05:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,4 +44,4 @@ if ingredients_list: # list will be displayed only if selected
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response)
 #st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=st.text(smoothiefroot_response.json(),use_container_width = true)
+sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width = true)
